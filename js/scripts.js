d45d2006c5d7bfe3dba28b66d26e3584753ca963
10EastSea <xgf6580@gmail.com>
2021-08-15 16:44:22 +0900

    ver 0.0.6 (css, js 파일 분화)

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Lotto Function
+function lotto() {
+    var lottoNumList = [];
+
+    for(var i=0; i<6; i++) {
+        var num = Math.floor(Math.random() * 45) + 1;
+
+        if(!(lottoNumList.find((x) => (x === num)))) lottoNumList.push(num);
+        else i--;
+    }
+
+    return lottoNumList;
+}
