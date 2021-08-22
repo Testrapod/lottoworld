19543c69c35db8db556fc32e6961996753794bcd
10EastSea <xgf6580@gmail.com>
2021-08-22 18:31:39 +0900

    ver 0.4.2 (카카오 API 초안 적용 2)

--- js/kakao_map.js
+++ js/kakao_map.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
     var map = new kakao.maps.Map(mapContainer, mapOption);
 
     // 장소 검색 객체를 생성합니다
-    var ps = new kakao.maps.services.Places(map);
+    var ps = new kakao.maps.services.Places();
 
     // 검색 결과 목록이나 마커를 클릭했을 때 장소명을 표출할 인포윈도우를 생성합니다
     var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
@@ -30,8 +30,14 @@ $(document).ready(function () {
             return false;
         }
 
+        var lat, lon;
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            lat = pos.coords.latitude;
+            lon = pos.coords.longitude;
+        });
+
         // 장소검색 객체를 통해 키워드로 장소검색을 요청합니다
-        ps.keywordSearch(keyword, placesSearchCB, { x: 127.121160, y: 37.438327, radius: 10000 });
+        ps.keywordSearch(keyword, placesSearchCB, { x: lon, y: lat, radius: 10000 });
     }
 
     // 장소검색이 완료됐을 때 호출되는 콜백함수 입니다
