ac4dcebcf9549cdc8f3bb6fb8854494cb601c3b3
10EastSea <xgf6580@gmail.com>
2021-08-29 14:15:58 +0900

    ver 0.6.5 (라이브리 댓글 기능)

--- js/comment_ready.js
+++ js/comment_ready.js
@@ -1,6 +1,4 @@
 /* comment.html document ready */
 $(document).ready(function () {
-    $.getJSON("https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=1", function (data) {
-        console.log(data);
-    });
+    
 });
